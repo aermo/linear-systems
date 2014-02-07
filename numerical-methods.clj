ce37d7614f9dab908d6ba44e4ca98c5a6a37274f
aermo <antti.ramo@gmail.com>
2014-02-07 20:04:41 +0200

    lu-factorializatoin created

--- numerical-methods.clj
+++ numerical-methods.clj
@@ -1,8 +1,8 @@
 ;(def A [[1 2 3 4][6 5 4 1][9 8 7 1][9 2 1 3]])
 ;(def A [[1 2 3][4 5 1][3 2 1]]) (def b [8 5 2])
-(def A [[1.0E-32 1][1 1]]) (def b [1 2])
-
-(defn pivot [A k]
+;(def A [[1.0E-32 1][1 1]]) (def b [1 2])
+(def A [[0.6 1.52 3.5][2 4 1][1 2.8 1]])
+(defn pivot1 [A k]
   (reduce 
     #(if (< (second %2) (nth (first %1) k))
       %1 
@@ -10,56 +10,153 @@
         (nth %1 (first %2))
         (concat (take (first %2) %1) (drop (inc (first %2)) %1))))
     A (map vector (range) (map #(nth % k) A))))
-(println (pivot A 1))
+;(println (pivot1 A 1))
 
-(defn pivot2 [A k]
-  (let [pivot-row (key (apply max-key val (zipmap (range) (map #(nth % k) A))))]
-    (concat 
-      [(nth A pivot-row)]
-      (take pivot-row A)
-      (drop (inc pivot-row) A))))
-(println (pivot2 A 1))
+(defn pivot [A k]
+  ; Find the row with the greatest value in the column k.
+  (let [p (->> 
+            (map #(first %) A)  ; Take column k.
+            ;(map #(nth % k) A)  ; Take column k.
+            (zipmap (range))    ; Add row index.
+            (apply max-key val) ; Take the key-value-pair with greatest value.
+            (key))]             ; Extract the key.
+  ; Move the row on the top
+    [(concat [(nth A p)] (take p A) (drop (inc p) A)) p]))
+;(println (pivot A 1))
 
-;(defn gaussian-elimination [A b]
-;  (let [n (count A)]
-;    (loop [A A b b k 0]
-;      (if (= k (dec n)) 
-;        [(triu A) b]
-;        (let [a (map #(/ % (nth k (nth k A))) (map #(nth % k) (drop (inc k) A)))]
-;          (recur (concat 
-;                    (take k A) 
-;                    (map #(drop (inc k) %) (drop (inc k) A)))
-;                 b
-;                 (inc k))))))
+;(defn backward-substitution [U c] 
+;; Solves a linear system 'Ux = c' by calculating the backward substitution.
+;    ; Take upper triangular matrix without zeros and cons vector c.
+;    (let [U (map
+;              #(cons %1 (take-last %3 %2)) 
+;              c U (iterate dec (count U)))]
+;      (reduce
+;        #(cons 
+;          (/ (- (first %2) (apply + (map * %1 (nnext %2)))) (second %2))
+;          %1)
+;        '() (reverse U))))
+;(println (backward-substitution [[2 4 1][0 0.8 0.5][0 0 3]] [-3.8 -0.4 9.6]))
+
+(defn backward-substitution-sparse [U c] 
+; Solves a linear system 'Ux = c' by calculating the backward substitution.
+    ; Take upper triangular matrix without zeros and cons vector c.
+    (let [U (map #(cons %1 %2) c U)]
+      (reduce
+        #(cons 
+          (/ (- (first %2) (apply + (map * %1 (nnext %2)))) (second %2))
+          %1)
+        '() (reverse U))))
+
+
+(defn backward-substitution1 [U c] 
+; Solves a linear system 'Ux = c' by calculating the backward substitution.
+    ; Take upper triangular matrix without zeros and cons vector c.
+    (let [U (map
+              #(cons %1 (take %3 (reverse %2))) 
+              c U (iterate dec (count U)))]
+      (reduce
+        #(cons 
+          (/ (- (first %2) (apply + (map * %1 (rest %2)))) (last %2))
+          %1)
+        '() (reverse U))))
+;(println (backward-substitution [[2 4 1][0 0.8 0.5][0 0 3]] [-3.8 -0.4 9.6]))
+
+(defn backward-substitution2 [U c] 
+; Backward substitution with a loop structure.
+  (loop [k (dec (count U)) y '()]
+    (if (< k 0)
+      y
+      (recur
+        (dec k)
+        (cons 
+          (/ 
+            (- 
+              (nth c k) 
+              (apply + (map * (take-last (- (dec (count U)) k) (nth U k)) y)))
+            (nth (nth U k) k)) y)))))
+;(println (backward-substitution2 [[2 4 1][0 0.8 0.5][0 0 3]] [-3.8 -0.4 9.6]))
+
 
 (defn gaussian-elimination [A b]
-  (letfn [(A-ki-kj [A]
+  (letfn [(elimination-step [A]
             (map 
               (fn [ai-jj]
                 (map 
                   #(- %1 (/ (* %2 (first ai-jj)) (first (first A))))
                   (rest ai-jj) (rest (first A)))) 
               (rest A)))
-          (recursive [A k]
+          (sub-ge [A k]
             (when-first [a1 A]
               (cons (concat (repeat k 0) a1)
-                    (recursive (A-ki-kj A) (inc k)))))]
-  (recursive (map #(conj %1 %2) A b) 0)))
-(println (gaussian-elimination A b))
+                    (sub-ge (elimination-step A) (inc k)))))]
+  (sub-ge (map #(conj (vec %1) %2) A b) 0)))
+;(println (gaussian-elimination A b))
 
 (defn gaussian-elimination-with-pivoting [A b]
-  (letfn [(A-ki-kj [A]
+  (letfn [(elimination-step [A]
             (map 
-              (fn [ai-jj]
+              (fn [ai]
                 (map 
-                  #(- %1 (/ (* %2 (first ai-jj)) (first (first A))))
-                  (rest ai-jj) (rest (first A)))) 
+                  #(- %1 (/ (* %2 (first ai)) (first (first A))))
+                  (rest ai) (rest (first A)))) 
               (rest A)))
-          (recursive [A k]
+          (sub-ge [A k]
             (when-first [a1 A]
               (cons (concat (repeat k 0) a1)
-                    (recursive (A-ki-kj (pivot A 0)) (inc k)))))]
-  (recursive (map #(conj %1 %2) A b) 0)))
-(println (gaussian-elimination-with-pivoting A b))
+                    (sub-ge (elimination-step (first (pivot A 0))) (inc k)))))]
+  (sub-ge (map #(conj (vec %1) %2) A b) 0)))
+;(println (gaussian-elimination-with-pivoting A b))
 
+(defn lu-factorialization [A]
+  (letfn [(elimination-step [A]
+            (map 
+              (fn [ai]
+                (let [m (/ (first ai) (first (first A)))]
+                  [(map #(- %1 (* %2 m)) (rest ai) (rest (first A))) m]))
+              (rest A)))
+          (sub-ge [A k p]
+            (when-first [a1 A]
+              (let [Ap (pivot A 0)
+                    Am (elimination-step (first Ap))]
+                (cons [(concat (repeat k 0) [1] (reverse (map second Am))) ; Li (transpose)
+                       (concat (repeat k 0) (first (first Ap)))     ; return Ui
+                       (nth p (second Ap))]                                ; pi
+                      (sub-ge (map first Am)  (inc k) (remove #(= (second Ap) %) p))))))]
+  (apply map vector (sub-ge A 0 (range (count A))))))
+(def LUp (lu-factorialization A))
 
+(defn lu-factorialization-sparse [A]
+  (letfn [(elimination-step [A]
+            (map 
+              (fn [ai]
+                (let [m (/ (first ai) (first (first A)))]
+                  [(map #(- %1 (* %2 m)) (rest ai) (rest (first A))) m]))
+              (rest A)))
+          (sub-ge [A k p]
+            (when-first [a1 A]
+              (let [Ap (pivot A 0)
+                    Am (elimination-step (first Ap))]
+                (cons [(reverse (map second Am)) ; multpliers of Li
+                       (first (first Ap))        ; values of Ui
+                       (nth p (second Ap))]      ; pivot pi
+                      (sub-ge (map first Am)  (inc k) (remove #(= (second Ap) %) p))))))]
+  (apply map vector (sub-ge A 0 (range (count A))))))
+
+(def LUp (lu-factorialization-sparse A))
+(println LUp)
+
+(defn lu-forward-substitution-sparse [L p b]
+  (let [b (map #(nth b %) p)]
+    (reduce
+      (fn [b k]
+        (concat 
+          (take k b) 
+          (map 
+            #(- %1 (* %2 (nth b (dec k)))) 
+            (drop k b) (nth L (dec k)))))
+      b (range 1 (count b)))))
+
+(def y (lu-forward-substitution-sparse (first LUp) (last LUp) [8.3 -3.8 -2.3]))
+(println y)
+
+(println (backward-substitution-sparse (second LUp) y))
--- performance-testing.clj
+++ performance-testing.clj
@@ -1,8 +1,26 @@
 (def n 2000) ; Matrix size
 (def k 1000) ; Index of the returned element - force lazy sequence to be evaluated.
 (def A (take n (repeatedly (fn [] (take n (repeatedly #(rand 10)))))))
-;(def v (take n (repeatedly #(rand 10))))
-(def v (range n))
+(def v (take n (repeatedly #(rand 10))))
+;(def v (range n))
 
-(time (nth (map #(apply + (map * % v)) A) k))
-(time (nth (map (fn [ai] (reduce #(+ %1 (apply * %2)) 0 (map vector v ai))) A) k))
+;; Random sequences are evaluated here.
+;(time (nth (map #(apply + (map * % v)) A) k))
+
+;; Performance comparison.
+;(time (nth 
+;  (map #(apply + (map * % v)) A)
+;  k))
+;(time (nth 
+;  (map (fn [ai] (reduce #(+ %1 (apply * %2)) 0 (map vector v ai))) A)
+;  k))
+;; --> Simple is better!
+
+
+
+(load-file "numerical-methods.clj")
+(time (nth (backward-substitution A v) k))
+
+(time (nth (backward-substitution A v) k))
+(time (nth (backward-substitution1 A v) k))
+(time (nth (backward-substitution2 A v) k))
